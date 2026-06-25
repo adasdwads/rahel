607eea4fe17ac0ac22ba9f7e776b550bcafc5a8b
adasdwads <adasdwads@users.noreply.github.com>
2026-06-25 17:58:31 +0400

    fix: update API URL to local network IP

--- mobile/lib/config/app_config.dart
+++ mobile/lib/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
   static const String appName = 'RAHEL';
-  static const String baseUrl = 'http://10.0.2.2:3000/api';
+  static const String baseUrl = 'http://192.168.0.44:3000/api';
 }
